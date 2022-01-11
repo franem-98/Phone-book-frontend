e3de976805b5bc3e470dde8089023943d7b31777
Frane Mišetić <frane.misetic@yahoo.com>
2022-01-11 13:34:39 +0100

    Simplified second counter and fixed navigation timeout problem.

--- src/components/Calling.jsx
+++ src/components/Calling.jsx
@@ -1,20 +1,39 @@
-import React from "react";
-import { useLocation, useParams } from "react-router-dom";
-import SecondCounter from "./SecondCounter";
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
 
 //add seconds counter
 
 function Calling() {
+  const [seconds, setSeconds] = useState(0);
+  const [callTimedOut, setCallTimedOut] = useState(false);
   const { id } = useParams();
-  const location = useLocation();
-  const contact = location.state;
+  const { state: contact } = useLocation();
+  const navigate = useNavigate();
+  const secondLimit = 35;
 
   const contactId = contact ? contact.name : id;
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setSeconds(seconds + 1);
+    }, 1000);
+
+    if (seconds === secondLimit) {
+      setCallTimedOut(true);
+      clearInterval(timer);
+      setTimeout(() => {
+        navigate(-1);
+      }, 4000);
+    }
+
+    return () => clearInterval(timer);
+  }, [seconds, setSeconds, navigate]);
 
   return (
     <div className="calling-page">
       <h1>Calling {contactId}...</h1>
-      <SecondCounter />
+      <h2>{seconds}</h2>
+      {callTimedOut && <p>Person is not answering. Please try again later.</p>}
     </div>
   );
 }
